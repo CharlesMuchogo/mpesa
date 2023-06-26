d8b96f741bd71ac67696c44721f40b74ca4e0ea4
charles <muchpaul2@gmail.com>
2023-06-26 22:30:13 +0300

    added mpesa xpress

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-import "main/c2b"
+import (
+	"main/c2b"
+)
 
-func main() {
-	
-c2b.StkPush("254758896593", 1, "https://baraka-97cb7-default-rtdb.firebaseio.com/mpesa")     
+func main() {	
+c2b.StkPush("254758896593", 1, "https://baraka-97cb7-default-rtdb.firebaseio.com/mpesa.json")     
  
 }
 
