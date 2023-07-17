d46d39f3817759e65f07024849e103392f46889d
charles <muchpaul2@gmail.com>
2023-07-18 00:26:49 +0300

    Added jwt authentication to the api

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"main/c2b"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"main/api"
+	"main/controllers"
+	"main/database"
+	"main/middlewares"
+	"main/utils"
 )
 
-func main() {	
-c2b.StkPush("254758896593", 1, "https://baraka-97cb7-default-rtdb.firebaseio.com/mpesa.json")     
- 
+func main() {
+	fmt.Println("Starting server")
+
+	// Initialize Database
+	database.Connect(utils.GoDotEnvVariable("databaseUrl"))
+	//database.Migrate()
+
+	// Initialize Router
+	router := initRouter()
+	router.Run(":8080")
 }
 
-
+func initRouter() *gin.Engine {
+	router := gin.Default()
+	apis := router.Group("/api")
+	{
+		apis.POST("/token", controllers.GenerateToken)
+		apis.POST("/user/register", controllers.RegisterUser)
+		secured := apis.Group("/secured").Use(middlewares.Auth())
+		{
+			secured.GET("/ping", controllers.Ping)
+			secured.POST("/mpesa-express", api.MpesaExpress)
+		}
+	}
+	return router
+}
